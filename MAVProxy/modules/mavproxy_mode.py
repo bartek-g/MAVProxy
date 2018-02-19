5a8cb713616b2ad502c63b6ad67d6bc0ddd35950
bartek-g <bartlomiej.gaska@apptimia.com>
2018-02-19 15:34:09 +0100

    SNDRIVERS-5 Implement NAV_LOITER_TURNS that works in guided mode (part 2) - user can enter "guided" or "guided_loiter" with extra turns parameter, - fractional turns are sent to autopilot without rounding

--- MAVProxy/modules/mavproxy_mode.py
+++ MAVProxy/modules/mavproxy_mode.py
@@ -51,12 +51,22 @@ class ModeModule(mp_module.MPModule):
     def cmd_guided(self, args):
         '''set GUIDED target'''
         arg_count = len(args)
-        if arg_count != 1 and arg_count != 3 and arg_count != 4:
-            print("Usage: guided ALTITUDE | guided LAT LON ALTITUDE | guided LAT LON ALTITUDE RADIUS")
+        if not arg_count == 1 and (3 > arg_count or arg_count > 5):
+            print("Usage: guided ALTITUDE | guided LAT LON ALTITUDE")
+            print("       guided LAT LON ALTITUDE RADIUS | guided LAT LON ALTITUDE RADIUS TURNS")
             return
 
         using_guided_loiter = False
-        if len(args) == 4:
+        loiter_turns = 0
+        if len(args) == 5:
+            latitude = float(args[0])
+            longitude = float(args[1])
+            altitude = float(args[2])
+            loiter_radius = float(args[3])
+            loiter_turns = float(args[4])
+            lat_lon = (latitude, longitude)
+            using_guided_loiter = True
+        elif len(args) == 4:
             latitude = float(args[0])
             longitude = float(args[1])
             altitude = float(args[2])
@@ -75,48 +85,64 @@ class ModeModule(mp_module.MPModule):
                 print("No map available")
                 return
             if lat_lon is None:
-                print("No map click position available")
+                print("No map click position available - click a new position")
                 return
             altitude = float(args[0])
 
         if not using_guided_loiter:
             print("Guided %s %s" % (str(lat_lon), str(altitude)))
-            self.master.mav.mission_item_send (self.settings.target_system,
-                                               self.settings.target_component,
-                                               0,
-                                               self.module('wp').get_default_frame(),
-                                               mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-                                               2, 0, 0, 0, 0, 0,
-                                               lat_lon[0], lat_lon[1], altitude)
+            self.master.mav.mission_item_send(
+                self.settings.target_system,
+                self.settings.target_component,
+                0,
+                self.module('wp').get_default_frame(),
+                mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+                2, 0, 0, 0, 0, 0,
+                lat_lon[0], lat_lon[1], altitude)
         else:
-            print("Guided loitering %s %s radius %s" % (str(lat_lon), str(altitude), str(loiter_radius)))
-            self.master.mav.mission_item_send (self.settings.target_system,
-                                               self.settings.target_component,
-                                               0,
-                                               self.module('wp').get_default_frame(),
-                                               mavutil.mavlink.MAV_CMD_NAV_LOITER_TURNS,
-                                               2, 0, 0, 0, loiter_radius, 0,
-                                               lat_lon[0], lat_lon[1], altitude)
+            if loiter_turns == 0:
+                print("Guided loitering %s %s radius %s" % (
+                    str(lat_lon), str(altitude), str(loiter_radius))
+                      )
+            else:
+                print("Guided loitering %s %s radius %s turns %s" % (
+                    str(lat_lon), str(altitude), str(loiter_radius), str(loiter_turns))
+                    )
+            self.master.mav.mission_item_send(
+                self.settings.target_system,
+                self.settings.target_component,
+                0,
+                self.module('wp').get_default_frame(),
+                mavutil.mavlink.MAV_CMD_NAV_LOITER_TURNS,
+                2, 0, loiter_turns, 0, loiter_radius, 0,
+                lat_lon[0], lat_lon[1], altitude)
 
     def cmd_guided_loiter(self, args):
         arg_count = len(args)
-        if arg_count != 1:
-            print("Usage: guided_loiter RADIUS")
+        if arg_count < 1 or arg_count > 2:
+            print("Usage: guided_loiter RADIUS | guided_loiter RADIUS TURNS")
             return
         try:
             lat_lon = self.module('map').click_position
         except Exception:
-            print("No map available")
+            print("No map available - click a new position")
             return
         altitude = self.mpstate.master().field('GLOBAL_POSITION_INT', 'relative_alt', 0) * 1.0e-3
         loiter_radius = float(args[0])
-        print("Guided loitering %s %s radius %s" % (str(lat_lon), str(altitude), str(loiter_radius)))
+        loiter_turns = 0
+        if arg_count == 2:
+            loiter_turns = float(args[1])
+            print("Guided loitering %s %s radius %s turns %s" % (
+                str(lat_lon), str(altitude), str(loiter_radius), str(loiter_turns))
+                  )
+        else:
+            print("Guided loitering %s %s radius %s" % (str(lat_lon), str(altitude), str(loiter_radius)))
         self.master.mav.mission_item_send(self.settings.target_system,
                                           self.settings.target_component,
                                           0,
                                           self.module('wp').get_default_frame(),
                                           mavutil.mavlink.MAV_CMD_NAV_LOITER_TURNS,
-                                          2, 0, 0, 0, loiter_radius, 0,
+                                          2, 0, loiter_turns, 0, loiter_radius, 0,
                                           lat_lon[0], lat_lon[1], altitude)
 
 
